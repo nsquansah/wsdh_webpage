1226660abdfd2a237a8fea4e2b80705c546a1723
nsquansah <ninaspioquansah@gmail.com>
2025-04-27 16:13:24 +0000

    hero section complete

--- my-newapp/src/components/HeroSection.js
+++ my-newapp/src/components/HeroSection.js
@@ -8,9 +8,12 @@ function heroSection(){
         <div className="hero-section">
             <img src={boy} alt="boy" className="hero-image"/>
             <div className="hero-text">
-                <h1>Welcome to My New App</h1>
-                <p>This is a simple hero section with an image and some text.</p>
-                <button className="hero-button">Get Started</button>
+                <h1 className='whatwedo'>From Farms to Tech</h1>
+                
+                <p className='summary1'>We support women through training in entrepreneurship,</p>
+                <p className='summary2'> farming, food processing, and technology </p>
+                <p className='summary3'>— driving change from the grassroots.</p>
+                <button className="btn neon-pulse"><span>Donate</span></button>
             </div>
         </div>
             );
